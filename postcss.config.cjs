1c76b56babd6a5580f16f7926dabf283b9baf1bd
alejandroRodriguez <iesalejandrorodriguezbanon@gmail.com>
2026-08-31 17:59:29 +0200

    remove un used vite/nodejs

--- postcss.config.cjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-
-const base = process.env.VITE_BASE ?? "/"
-
-export default defineConfig({
-  plugins: [svelte()],
-  base,
-})
